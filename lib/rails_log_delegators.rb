2945de6b49a98fb4848a83f9a14b420ba84c9990
Laszlo Papp <lpapp@zendesk.com>
2014-08-26 00:24:57 +0100

    Add basic functionality

--- lib/rails_log_delegators.rb
+++ lib/rails_log_delegators.rb
@@ -1,5 +1,46 @@
 require "rails_log_delegators/version"
 
+#
+# Creates delegators for Rails.logger.(error|warning|info|debug)
+# methods.
+#
+# Example:
+#
+#    class DemoController  < ActionController::Base
+#      def index
+#        info "Demo controller #index has been called", parameters: params
+#      end
+#    end
+#
 module RailsLogDelegators
-  # Your code goes here...
+  [:error, :warning, :info, :debug].each do |severity|
+    if defined? Rails.logger
+      define_method(:logging_out) { |severity, *args| Rails.logger.send(severity, *args) }
+    else
+      define_method(:logging_out) { |severity, *args| puts "[#{severity}] " + args.join("") }
+    end
+
+    define_method severity do |message, options = []|
+      logging_out(severity, RailsLogDelegators::format_message(message, options))
+    end
+  end
+
+  private
+
+  def self.format_message(message, options = {})
+    if !options.empty?
+      message + "; " + options.map { |k, v| "#{k}='#{v}'"}.join(", ")
+    else
+      message
+    end
+  end
 end
+
+class Object
+  include RailsLogDelegators
+end
+
+class Class
+  include RailsLogDelegators
+end
+
--- rails_log_delegators.gemspec
+++ rails_log_delegators.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = RailsLogDelegators::VERSION
   spec.authors       = ["Laszlo Papp"]
   spec.email         = ["lpapp@zendesk.com"]
-  spec.summary       = %q{TODO: Write a short summary. Required.}
-  spec.description   = %q{TODO: Write a longer description. Optional.}
+  spec.summary       = %q{Delegates log methods to Rails.logger}
+  spec.description   = %q{More comfortable way to write log messages}
   spec.homepage      = ""
   spec.license       = "MIT"
 
@@ -18,6 +18,7 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  spec.add_development_dependency "bundler", "~> 1.7"
+  spec.add_development_dependency "bundler", "~> 1.6"
   spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "minitest", "5.4.0"
 end
